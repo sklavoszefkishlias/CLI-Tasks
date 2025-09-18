4c28f8bbfc0eb67990107d541943a80e6b67a2e4
sklavoszefkishlias <sklavoszefkishlias@gmail.com>
2025-09-18 18:12:57 +0300

    added the add function

--- main.py
+++ main.py
@@ -1,33 +1,43 @@
-import datetime
 import json
-
+from prime_functions import add
 
 if __name__ == "__main__":
-    
+    empty_flag=0
+    total_tasks = 0
+    tasks_data ={} 
+    #This checks if taks.json is empty and initialises the index of the last tasks
     with open("tasks.json", mode="r", encoding="utf-8") as read_file:
         first_char=read_file.read(1)
         if not first_char:
-            total_tasks= 0
-        else:
-            tasks_data = json.load(read_file)
-            total_tasks = len(tasks_data)
+            empty_flag=1
+    
+    if empty_flag == 0:
+        with open("tasks.json", mode="r", encoding="utf-8") as read_file:
+                tasks_data = json.load(read_file)
+                total_tasks = len(tasks_data)
     print(total_tasks)
-    '''while True:
-        x = datetime.datetime.now()
-        command , parameters = input("").split(" ",1)
 
-        if command == "exit":
-            break
-        elif command == "add":
-            add(parameters)
-        elif command == "update":
-            update(parameters)
-        elif command == "mark":
-            mark(parameters)
-        elif command == "delete":
-            delete(parameters)
-        elif command =="view":
-            view(parameters)
-    '''
+
+    while True:
+        f_input = input("cli task ")
+        if f_input == "exit":
+             break
+        else:
+             
+            command , parameters = f_input.split(" ",1)
+
+            if command == "exit":
+                break
+            elif command == "add":
+                add(parameters,total_tasks)
+            '''elif command == "update":
+                update(parameters)
+            elif command == "mark":
+                mark(parameters)
+            elif command == "delete":
+                delete(parameters)
+            elif command =="view":
+                view(parameters)'''
+        
 
     print("Bye Bye")
